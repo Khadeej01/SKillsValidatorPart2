046e1b766a2baaef5cb8e0eacb3e569e75bc49fc
saidamst2003 <saida2003mst@gmail.com>
2025-07-14 12:08:21 +0100

    j'ai creer test de fonction inscrire

--- Apprenant-Service/src/test/java/com/Skills_Validator/Apprenant_Service/service/ApprenantServiceTest.java
+++ Apprenant-Service/src/test/java/com/Skills_Validator/Apprenant_Service/service/ApprenantServiceTest.java
@@ -1,20 +1,47 @@
 package com.Skills_Validator.Apprenant_Service.service;
 
+import com.Skills_Validator.Apprenant_Service.Repository.ApprenantRepository;
+import com.Skills_Validator.Apprenant_Service.dto.ApprenantDTO;
+import com.Skills_Validator.Apprenant_Service.model.Apprenant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
+
+
 @SpringBootTest
 class ApprenantServiceTest {
-@Autowired
- private ApprenantService ApprenantService;
-    @Test
-    void inscrire() {
 
-    }
+   @Autowired
+   private ApprenantService apprenantService;
+   @Autowired
+   private ApprenantRepository apprenantRepository;
 
-    @Test
-    void getApprenantById() {
-    }
+   @Test
+   void testInscrire_shouldSaveAndReturnApprenantDTO() {
+      // Préparation du DTO
+      ApprenantDTO dto = new ApprenantDTO();
+      dto.setNom("Fatima");
+      dto.setPrenom("Zahra");
+      dto.setEmail("fatima@test.com");
+      dto.setMotDePasse("motdepasse");
+
+      // Appel de la méthode à tester
+      ApprenantDTO result = apprenantService.inscrire(dto);
+
+      // Vérifications
+      assertNotNull(result);
+      assertNotNull(result.getId());
+      assertEquals("fatima@test.com", result.getEmail());
+
+      // Vérification dans la base H2
+      Apprenant saved = apprenantRepository.findById(result.getId()).orElse(null);
+      assertNotNull(saved);
+      assertEquals("Fatima", saved.getNom());
+   }
+
+
+
+
 }
