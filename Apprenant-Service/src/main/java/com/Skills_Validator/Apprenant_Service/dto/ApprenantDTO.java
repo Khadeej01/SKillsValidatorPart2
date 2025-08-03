546a79b2684dd748896c867414511208492ecf9a
saidamst2003 <saida2003mst@gmail.com>
2025-08-03 14:21:35 +0100

    j'ai

--- Apprenant-Service/src/main/java/com/Skills_Validator/Apprenant_Service/dto/ApprenantDTO.java
+++ Apprenant-Service/src/main/java/com/Skills_Validator/Apprenant_Service/dto/ApprenantDTO.java
@@ -33,4 +33,60 @@ public class ApprenantDTO {
 
     private LocalDateTime dateInscription;
     private List<RenduDto> rendus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "Le nom est obligatoire") String getNom() {
+        return nom;
+    }
+
+    public void setNom(@NotBlank(message = "Le nom est obligatoire") String nom) {
+        this.nom = nom;
+    }
+
+    public @NotBlank(message = "Le prénom est obligatoire") String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(@NotBlank(message = "Le prénom est obligatoire") String prenom) {
+        this.prenom = prenom;
+    }
+
+    public @Email(message = "Email invalide") @NotBlank(message = "L'email est obligatoire") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Email(message = "Email invalide") @NotBlank(message = "L'email est obligatoire") String email) {
+        this.email = email;
+    }
+
+    public @Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères") String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(@Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères") String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public LocalDateTime getDateInscription() {
+        return dateInscription;
+    }
+
+    public void setDateInscription(LocalDateTime dateInscription) {
+        this.dateInscription = dateInscription;
+    }
+
+    public List<RenduDto> getRendus() {
+        return rendus;
+    }
+
+    public void setRendus(List<RenduDto> rendus) {
+        this.rendus = rendus;
+    }
 }
